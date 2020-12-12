620cbcf76ea951aed7a98a13a2edf2a3df30aa4c
Tim Rybicki <t.rybicki@gmail.com>
2020-12-12 17:24:18 -0500

    refactor(components): Create component files

---
+++ src/components/repos/RepoPage.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+function RepoPage(props) {
+  const repoRef = useRef(null);
+  // const [history, setHistory] = useState([]);
+  const { repo } = props.match.params;
+
+  const getData = async () => {};
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div ref={repoRef}>
+      <h1>{repo}</h1>
+    </div>
+  );
+}
+
+RepoPage.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repo: PropTypes.string,
+    }),
+  }),
+};
+
+export default RepoPage;
