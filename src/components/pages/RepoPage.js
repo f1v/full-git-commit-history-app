e90d39befc0c48daef7c2c30aed2e1e01b1bb8cb
David Ko <david.holy.ko@gmail.com>
2021-01-11 16:23:31 -0500

    feat(repo-page): get data if not populated by user page (#29)

--- src/components/pages/RepoPage.js
+++ src/components/pages/RepoPage.js
@@ -1,35 +1,40 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { userCommitHistoryState } from '../../recoil/atoms/userCommitHistoryState';
+import API from '../../utils/api';
+import { parseRepoData } from '../../utils/github-data-parser';
 
 export const RepoPage = ({ match }) => {
-  const userCommitHistory = useRecoilValue(userCommitHistoryState);
   const { user, repo } = match.params;
+  const [commitHistory, setUserCommitHistory] = useRecoilState(
+    userCommitHistoryState,
+  );
 
   const getData = async () => {
-    // TODO: get Data for that specific user/repo and place in userCommitHistoryState
+    const rawRepoData = await API.getRepoCommitHistory({
+      owner: user,
+      repo,
+    });
+    const repoCommits = parseRepoData(rawRepoData);
+    setUserCommitHistory({ ...commitHistory, [user]: { [repo]: repoCommits } });
   };
 
   useEffect(() => {
-    if (!userCommitHistory[user]) {
-      // TODO: if user doesnt exist we have to create the user key
+    if (!commitHistory[user] || !commitHistory[user][repo]) {
+      getData();
     }
-
-    if (!userCommitHistory[user][repo]) {
-      // TODO: if repo data doesn't exist, we need to look it up
-    }
-
-    getData();
   }, []);
 
   return (
     <div>
       <h1>{repo}</h1>
       <h1>{user}</h1>
-      {userCommitHistory[user][repo].map((commitData) => {
-        return <div key={commitData.sha}>{commitData.commit.message}</div>;
-      })}
+      {commitHistory[user] && commitHistory[user][repo]
+        ? commitHistory[user][repo].map((commitData) => {
+            return <div key={commitData.sha}>{commitData.commit.message}</div>;
+          })
+        : null}
     </div>
   );
 };
