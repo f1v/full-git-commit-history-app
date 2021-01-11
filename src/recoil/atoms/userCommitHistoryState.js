6d374cd218367a6222c9fa9766127e009fe902bb
David Ko <david.holy.ko@gmail.com>
2021-01-11 13:12:22 -0500

    fix typo

--- src/recoil/atoms/userCommitHistoryState.js
+++ src/recoil/atoms/userCommitHistoryState.js
@@ -9,6 +9,6 @@ export const userCommitHistoryState = atom({
   key: 'userCommitHistoryState',
   default: {},
   persistence_UNSTABLE: {
-    type: 'userRepoState',
+    type: 'userCommitHistoryState',
   },
 });
