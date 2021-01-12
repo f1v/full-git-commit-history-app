e21aead03ce88c12cdb2249e5da008c41c271487
David Ko <david.holy.ko@gmail.com>
2021-01-12 10:43:11 -0500

    feat(app): add simple footer (#41)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes } from './routes/Routes';
+import { Footer } from './main/Footer';
 
 function App() {
   return (
     <div className="app-wrapper">
       <Routes />
+      <Footer />
     </div>
   );
 }
