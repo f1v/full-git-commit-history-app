cdaac1c2de2ba2280d899f6dc068c45880d33a3a
Tim Rybicki <t.rybicki@gmail.com>
2021-01-12 11:34:10 -0500

    Add switch

--- src/utils/repoFilters.js
+++ src/utils/repoFilters.js
@@ -1,15 +1,16 @@
 import moment from 'moment';
 
 export const filterRepos = (repos, filterType) => {
-  if (filterType === 'alphabetical') {
-    return repos; // repos is alphabetical by default
-  }
-  if (filterType === 'lastUpdated') {
-    return repos
-      .slice() // repos is read-only so make a copy first
-      .sort((a, b) => moment(b.pushedAt).diff(moment(a.pushedAt)));
-  }
-  if (filterType === 'numStars') {
-    return repos.slice().sort((a, b) => b.numStars - a.numStars);
+  switch (filterType) {
+    case 'alphabetical':
+      return repos; // repos are alphabetical by default
+    case 'lastUpdated':
+      return repos
+        .slice() // repos is read-only so make a copy first
+        .sort((a, b) => moment(b.pushedAt).diff(moment(a.pushedAt)));
+    case 'numStars':
+      return repos.slice().sort((a, b) => b.numStars - a.numStars);
+    default:
+      return repos;
   }
 };
