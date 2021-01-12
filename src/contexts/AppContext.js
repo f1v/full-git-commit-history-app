7c63e05f4bf2cf42ccda26adaee5e19c612773ce
David Ko <david.holy.ko@gmail.com>
2021-01-12 10:58:50 -0500

    remove lightmode from app context

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -6,15 +6,11 @@ export const AppContext = React.createContext(null);
 // This context provider is passed to any component requiring the context
 export const AppProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
-  // TODO: implement lightMode
-  const [isLightMode, setIsLightMode] = useState(false);
 
   return (
     <AppContext.Provider
       value={{
-        isLightMode,
         isLoading,
-        setIsLightMode,
         setIsLoading,
       }}
     >
