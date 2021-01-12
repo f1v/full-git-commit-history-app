a12c5a75980323bc86c7d57e38865be5a8195167
David Ko <david.holy.ko@gmail.com>
2021-01-12 10:34:32 -0500

    feat(app): add simple footer

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import { Link, Text } from '@chakra-ui/react';
 
 export const Footer = () => {
-  // TODO
-  return <footer></footer>;
+  const projectLink = 'https://github.com/f1v/full-git-commit-history-app';
+  const f1vLink = 'https://f1v.com/';
+
+  return (
+    <footer style={{ padding: '20px' }}>
+      <Text fontSize="12px">
+        This{' '}
+        <Link href={projectLink} isExternal>
+          project
+        </Link>{' '}
+        was made by{' '}
+        <Link href={f1vLink} isExternal>
+          F1V
+        </Link>{' '}
+        for our hackathon 2021
+      </Text>
+    </footer>
+  );
 };
